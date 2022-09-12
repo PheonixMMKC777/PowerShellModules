d42d33228042a3403902d11ec0708e6cd95ca3dc
That Blonde Kid <72944557+PheonixMMKC777@users.noreply.github.com>
2022-09-12 13:55:24 -0500

    Add files via upload

--- get-subnet/get-subnet.psm1
+++ get-subnet/get-subnet.psm1
@@ -21,21 +21,23 @@ function ClassC {
 Write-host "===================================" -f $color2
 
 $IpAddress = Read-Host "IP Address"
-[byte]$cidr = Read-Host "CIDR"
+$cidr = Read-Host "CIDR or Mask"
+
+
 
 #a b & c are buffers
 $a,$b,$c,[byte]$ipoctet = $IpAddress.Split('.')
 
 Write-host "===================================" -f $color2
 #Class C Working
-If ($cidr -eq "25") {$Mask = 128;$subnets = 2;$hosts = 128}
-If ($cidr -eq "26") {$Mask = 192;$subnets = 4;$hosts = 64}
-If ($cidr -eq "27") {$Mask = 224;$subnets = 8;$hosts = 32}
-If ($cidr -eq "28") {$Mask = 240;$subnets = 16;$hosts = 16}
-If ($cidr -eq "29") {$Mask = 248;$subnets = 32;$hosts = 8}
-If ($cidr -eq "30") {$Mask = 252;$subnets = 64;$hosts = 4}
-If ($cidr -eq "31") {$Mask = 254;$subnets = 0;$hosts = 2}
-If ($cidr -eq "32") {$Mask = 255;$subnets = 0;$hosts = 1}
+If (($cidr -eq "25")-or($cidr -eq "255.255.255.128")) {$Mask = 128;$subnets = 2;$hosts = 128;$cidr = "25";$NBits = 1}
+If (($cidr -eq "26")-or($cidr -eq "255.255.255.192")) {$Mask = 192;$subnets = 4;$hosts = 64;$cidr = "26";$NBits = 2}
+If (($cidr -eq "27")-or($cidr -eq "255.255.255.224")) {$Mask = 224;$subnets = 8;$hosts = 32;$cidr = "27";$NBits = 3}
+If (($cidr -eq "28")-or($cidr -eq "255.255.255.240")) {$Mask = 240;$subnets = 16;$hosts = 16;$cidr = "28";$NBits = 4}
+If (($cidr -eq "29")-or($cidr -eq "255.255.255.248")) {$Mask = 248;$subnets = 32;$hosts = 8;$cidr = "29";$NBits = 5}
+If (($cidr -eq "30")-or($cidr -eq "255.255.255.252")) {$Mask = 252;$subnets = 64;$hosts = 4;$cidr = "30";$NBits = 6}
+
+$Hbits = 8 - $NBits
 
 [byte]$RangeStart = $Mask -band $ipoctet
 [byte]$range = $RangeStart / $hosts
@@ -47,13 +49,15 @@ $RangeStart++
 $rangeEnd--
 Write-Host ""
 Write-Host "Current Subnet: $Range" -f $Color
-Write-Host "Subnet ID at: $a.$b.$c.$NetworkID" -f $Color
+Write-Host "Network ID at: $a.$b.$c.$NetworkID" -f $Color
 Write-Host "Useable Hosts Begin at: $a.$b.$c.$RangeStart" -f $Color
 Write-Host "Useable Hosts End at: $a.$b.$c.$rangeEnd" -f $Color
 Write-Host "Subnet Broadcast ID at: $a.$b.$c.$BroadcastID" -f $Color
 Write-Host "Subnet Mask is: 255.255.255.$Mask" -f $Color
 Write-Host "IP's Per Subnet: $hosts" -f $Color
-Write-Host "CIDR: $CIDR" -f $Color
+Write-Host "CIDR: /$CIDR" -f $Color
+Write-Host "Network Bits: $Nbits" -f $Color
+Write-Host "Host Bits: $Hbits" -f $Color
 Write-Host ""
 }
 
@@ -61,32 +65,32 @@ Write-Host ""
 function ClassB {
 Write-host "===================================" -f $color2
 $IpAddress = Read-Host "IP Address"
-[byte]$cidr = Read-Host "CIDR"
+$cidr = Read-Host "CIDR or Mask"
+
+
 
 #a & b are buffers
 $a,$b,[byte]$ipoctet, [byte]$ipoctet2 = $IpAddress.Split('.')
 
 
-If ($cidr -eq "17") {$Mask = 128;$Mask2 = 0;$subnets = 2;$Classhosts = 128;$hosts = 32768}
-If ($cidr -eq "18") {$Mask = 192;$Mask2 = 0;$subnets = 4;$Classhosts = 64;$hosts = 16384}
-If ($cidr -eq "19") {$Mask = 224;$Mask2 = 0;$subnets = 8;$Classhosts = 32;$hosts = 8192}
-If ($cidr -eq "20") {$Mask = 240;$Mask2 = 0;$subnets = 16;$Classhosts = 16;$hosts = 4096}
-If ($cidr -eq "21") {$Mask = 248;$Mask2 = 0;$subnets = 32;$Classhosts = 8;$hosts = 2048}
-If ($cidr -eq "22") {$Mask = 252;$Mask2 = 0;$subnets = 64;$Classhosts = 4;$hosts = 1024}
-If ($cidr -eq "23") {$Mask = 254;$Mask2 = 0;$subnets = 128;$Classhosts = 2;$hosts = 512}
-If ($cidr -eq "24") {$Mask = 255;$Mask2 = 0;$subnets = 256;$Classhosts = 1;$hosts = 256}
+If (($cidr -eq "17")-or($cidr -eq "255.255.128.0"))  {$Mask = 128;$Mask2 = 0;$subnets = 2;$Classhosts = 128;$hosts = 32768;$cidr = "17";$NBits = 1}
+If (($cidr -eq "18")-or($cidr -eq "255.255.192.0")) {$Mask = 192;$Mask2 = 0;$subnets = 4;$Classhosts = 64;$hosts = 16384;$cidr = "18";$NBits = 2}
+If (($cidr -eq "19")-or($cidr -eq "255.255.224.0")) {$Mask = 224;$Mask2 = 0;$subnets = 8;$Classhosts = 32;$hosts = 8192;$cidr = "19";$NBits = 3}
+If (($cidr -eq "20")-or($cidr -eq "255.255.240.0")) {$Mask = 240;$Mask2 = 0;$subnets = 16;$Classhosts = 16;$hosts = 4096;$cidr = "20";$NBits = 4}
+If (($cidr -eq "21")-or($cidr -eq "255.255.248.0")) {$Mask = 248;$Mask2 = 0;$subnets = 32;$Classhosts = 8;$hosts = 2048;$cidr = "21";$NBits = 5}
+If (($cidr -eq "22")-or($cidr -eq "255.255.252.0")) {$Mask = 252;$Mask2 = 0;$subnets = 64;$Classhosts = 4;$hosts = 1024;$cidr = "22";$NBits = 6}
+If (($cidr -eq "23")-or($cidr -eq "255.255.254.0")) {$Mask = 254;$Mask2 = 0;$subnets = 128;$Classhosts = 2;$hosts = 512;$cidr = "23";$NBits = 7}
+If (($cidr -eq "24")-or($cidr -eq "255.255.255.0")) {$Mask = 255;$Mask2 = 0;$subnets = 256;$Classhosts = 1;$hosts = 256;$cidr = "24";$NBits = 8}
 
 
 
-If ($cidr -eq "25") {$Mask = 255;$Mask2 = 128;$subnets = 512;$hosts = 128}
-If ($cidr -eq "26") {$Mask = 255;$Mask2 = 192;$subnets = 1024;$hosts = 64}
-If ($cidr -eq "27") {$Mask = 255;$Mask2 = 224;$subnets = 2048;$hosts = 32}
-If ($cidr -eq "28") {$Mask = 255;$Mask2 = 240;$subnets = 4096;$hosts = 16}
-If ($cidr -eq "29") {$Mask = 255;$Mask2 = 248;$subnets = 8192;$hosts = 8}
-If ($cidr -eq "30") {$Mask = 255;$Mask2 = 252;$subnets = 16384;$hosts = 4}
-If ($cidr -eq "31") {$Mask = 255;$Mask2 = 254;$subnets = 0;$hosts = 2}
-If ($cidr -eq "32") {$Mask = 255;$Mask2 = 255;$subnets = 0;$hosts = 1}
-
+If (($cidr -eq "25")-or($cidr -eq "255.255.255.128")) {$Mask = 255;$Mask2 = 128;$subnets = 512;$hosts = 128;$cidr = "25";$NBits = 9}
+If (($cidr -eq "26")-or($cidr -eq "255.255.255.192")) {$Mask = 255;$Mask2 = 192;$subnets = 1024;$hosts = 64;$cidr = "26";$NBits = 10}
+If (($cidr -eq "27")-or($cidr -eq "255.255.255.224")) {$Mask = 255;$Mask2 = 224;$subnets = 2048;$hosts = 32;$cidr = "27";$NBits = 11}
+If (($cidr -eq "28")-or($cidr -eq "255.255.255.240")) {$Mask = 255;$Mask2 = 240;$subnets = 4096;$hosts = 16;$cidr = "28";$NBits = 12}
+If (($cidr -eq "29")-or($cidr -eq "255.255.255.248")) {$Mask = 255;$Mask2 = 248;$subnets = 8192;$hosts = 8;$cidr = "29";$NBits = 13}
+If (($cidr -eq "30")-or($cidr -eq "255.255.255.252")) {$Mask = 255;$Mask2 = 252;$subnets = 16384;$hosts = 4;$cidr = "30";$NBits = 14}
+$Hbits = 16 - $NBits
 
 [int]$rangeStart = $Mask -band $ipoctet
 [int]$rangeStart2 = $Mask2 -band $ipoctet2
@@ -130,14 +134,16 @@ $rangeEnd2--
 Write-host "===================================" -f $color2
 Write-Host ""
 Write-Host "Current Subnet: $Range" -f $Color
-Write-Host "Subnet Network ID: $a.$b.$Rangestart.$NetworkID" -f $Color
+Write-Host "Network ID: $a.$b.$Rangestart.$NetworkID" -f $Color
 Write-Host "Useable Hosts Begin at: $a.$b.$Rangestart.$rangestart2" -f $Color
 Write-Host "Useable Hosts End at: $a.$b.$RangeEnd.$rangeEnd2" -f $Color
 Write-Host "Subnet Broadcast ID: $a.$b.$rangeEnd.$BroadcastID" -f $Color
 Write-Host "Subnet Mask: 255.255.$Mask.$Mask2" -f $Color
 Write-Host "Subnets Available: $subnets" -f $Color
 Write-Host "IP's Per Subnet: $hosts" -f $Color
-Write-Host "CIDR: $CIDR" -f $Color
+Write-Host "CIDR: /$CIDR" -f $Color
+Write-Host "Network Bits: $Nbits" -f $Color
+Write-Host "Host Bits: $Hbits" -f $Color
 Write-Host ""
 }
 
